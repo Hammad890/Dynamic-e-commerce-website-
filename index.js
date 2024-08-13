92cd442366ae1a65d361ab819cec27521dd0f5ac
Hammad890 <hammadbro6@gmail.com>
2024-08-13 11:57:00 +0500

    updated

--- index.js
+++ index.js
@@ -51,49 +51,9 @@ const fProducts = [{productId:1, title: "JANAN", Description: "Lorem ipsum dolor
 {productId:2, title: "JANAN", Description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat alias officiis distinctio quo dolor quae possimus. Et, ab sapiente. Quibusdam quas quia aspernatur! Dolorum voluptatibus possimus natus autem consequuntur necessitatibus.",Image: "https://www.junaidjamshed.com/media/catalog/product/j/a/janan_sport_1__1.jpg",isFeatured: true}]
 for(let product of fProducts){
     let mainDiv= document.createElement('div')
-<<<<<<< HEAD
     mainDiv.classList.add('col-md-6', 'col-lg-4', 'mb-4');
     let card = document.createElement('div');
     card.classList.add('card', 'h-100', 'text-center', 'shadow-sm'); 
-=======
-    
-    mainDiv.style.backgroundColor= 'antiquewhite'
-    mainDiv.style.border='4px solid darkcyan'
-    mainDiv.style.margin='0 10px';
-    mainDiv.style.textAlign = 'center';
-    let headingText= document.createTextNode(product.title)
-    let heading= document.createElement('h2')
-    heading.appendChild(headingText) 
-    heading.style.color= 'darkcyan'
-    heading.style.textAlign=''
-    mainDiv.appendChild(heading);
-    if(product.isFeatured){
-        let image= document.createElement('img')
-        image.setAttribute('src',product.Image)
-        let imageDiv= document.createElement('div')
-        imageDiv.appendChild(image)
-        image.style.width= '200px'
-        image.style.height= '200px'
-        mainDiv.appendChild(imageDiv);
-    }
-    let DescriptionText= document.createTextNode(product.Description)
-    let description= document.createElement('p')
-    description.appendChild(DescriptionText)
-    mainDiv.appendChild(description);
-    let button= document.createElement('button')
-    button.style.color= 'rgb(184, 245, 245)'
-    let buttonText= document.createTextNode("See More")
-    button.appendChild(buttonText)
-    button.addEventListener("click",button);
-    mainDiv.appendChild(button);
-    let productDiv = document.createElement('div')
-    productDiv.appendChild(mainDiv)
-    feature.appendChild(productDiv);
-   
-}
-});
->>>>>>> f50dfba11b0e445908618f38e4a636b6b033ddb1
-
     let heading = document.createElement('h2');
     heading.classList.add('card-title-1', 'text-darkcyan');
     heading.textContent = product.title;
